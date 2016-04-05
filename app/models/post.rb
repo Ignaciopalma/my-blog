ad340ad7cbb9cedfb2f3bb46deca403ff1073caf
Ignacio Palma <ignacio.palma.@live.com>
2016-04-05 01:09:59 -0300

    Arreglando error en paperclip para que muestre imagenes en direccion original

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 	has_many :assets
-	has_attached_file :image, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, 
+		styles: { original: {convert_options: '-auto-orient'},
+			      medium: "400x400>", 
+			      thumb: "100x100>" }, 
+		default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
     accepts_nested_attributes_for :assets, :allow_destroy => true
 end
